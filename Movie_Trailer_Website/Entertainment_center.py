606a894d69de171d9d3540228b4b0b0911767dd1
panzhiwei <panzw1105@gmail.com>
2017-08-23 15:11:37 -0400

    First commit

---
+++ Movie_Trailer_Website/Entertainment_center.py
@@ -0,0 +1,7 @@
+import media
+
+toy_story = media.Movie("Toy Story", "Toys Come to Life","https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwi4tJG_9evVAhUi74MKHSNpD8oQjRwIBw&url=http%3A%2F%2Fpixar.wikia.com%2Fwiki%2FToy_Story&psig=AFQjCNEIhRFetiGwzu4a4wUZUHhyFF9LLQ&ust=1503528232222276", "https://www.youtube.com/watch?v=KYz2wyBy3kc" )
+
+print(toy_story.title)
+print(toy_story.storyline)
+toy_story.show_trailer()
